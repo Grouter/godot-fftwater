0315bfa3d84283cf3ba4cfe631513665d73d04fd
Grouter <matus.perfekt@gmail.com>
2019-01-27 22:13:35 +0100

    increased time speed (faster more natural simulation)

--- godotProject/Water.gd
+++ godotProject/Water.gd
@@ -14,14 +14,15 @@ func _ready():
 	tessendorf.init(128)	# ftt resolution
 	tessendorf.amplitude = 15.0
 	tessendorf.wind_speed = 31.0
-	tessendorf.lambda = -1.0
+	tessendorf.length = 500.0
+	tessendorf.lambda = -0.9
 	tessendorf.smoothing = 2.0
 	tessendorf.wind_direction = Vector2(1.0, 0.0)
 	tessendorf.calculate()	# calculate according to variables above this
 
 func _process(delta):
 	var t = OS.get_ticks_msec() - start_time
-	tessendorf.update(t / 500.0, mdt, mat)
+	tessendorf.update(t / 400.0, mdt, mat)
 	self.mesh.surface_remove(0)
 	mdt.commit_to_surface(self.mesh)
 	
